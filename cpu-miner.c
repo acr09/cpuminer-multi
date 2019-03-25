e0c5cd1d839237565795a761bd23334e2c997e30
acr09 <32530769+acr09@users.noreply.github.com>
2019-03-25 15:20:48 -0700

    Update cpu-miner.c
    Test to see if I can make some changes to the file

--- cpu-miner.c
+++ cpu-miner.c
@@ -3414,8 +3414,8 @@ static int thread_create(struct thr_info *thr, void* func)
 
 static void show_credits()
 {
-	printf("** " PACKAGE_NAME " " PACKAGE_VERSION " by tpruvot@github **\n");
-	printf("BTC donation address: 1FhDPLPpw18X4srecguG3MxJYe4a1JsZnd (tpruvot)\n\n");
+	printf("** " PACKAGE_NAME " " PACKAGE_VERSION " by andy@helloworld **\n");
+	printf("BTC donation address: 1PmXv1LdbHztTYRH4ej2yJeL9zrPujN7NM (andy)\n\n");
 }
 
 void get_defconfig_path(char *out, size_t bufsize, char *argv0);
